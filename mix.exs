a18dbebecb5256d15cdc0cb50bc3a3cfb278ff68
Yaroslav Rogov <yrogov@adwirk.com>
2020-04-13 13:29:12 +0300

    Switch to temporary fork of rustler to use toml 0.6.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Wootheex.MixProject do
   def project do
     [
       app: :wootheex,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -23,9 +23,7 @@ defmodule Wootheex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.21.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:rustler, git: "git@bitbucket.org:pixel-media/rustler.git"}
     ]
   end
 
